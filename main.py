357df070c99b5ef952eb77c8f39166d62e89317d
sadko123 <115405702+sadko123@users.noreply.github.com>
2022-12-25 19:30:18 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QListWidget, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QListWidget, QHBoxLayout, QVBoxLayout, QFileDialog
+import os
+from PIL import Image
+
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+
+class ImageProcessor():
+    def __init__(self):
+        self.image = None
+        self.full_path = None
+    def loadImage(self, dir, name):
+        self.full_path = os.path.join(dir, name)
+    def showImage(self):
+        lbl_image.hide()
+        pixmapimage = QPixmap(self.full_path)
+        w, h = lbl_image.width(), lbl_image.height()
+        pixmapimage = pixmapimage.scaled(w, h, Qt.KeepAspectRatio)
+        lbl_image.setPixmap(pixmapimage)
+        lbl_image.show()
+
+
 app = QApplication([])
 main_win = QWidget()
 main_win.setWindowTitle('Редактор изображений')
+main_win.resize(700, 500)
+
+
 
 btn_select_dir = QPushButton('Выбрать папку')
 list_widget_images = QListWidget()
@@ -33,6 +57,40 @@ layout_main.addLayout(layout_image, 80)
 
 main_win.setLayout(layout_main)
 
+current_image = ImageProcessor()
+
+current_dir = ''
+
+def showFileNames():
+    print('открываем папку')
+    global current_dir
+    current_dir = QFileDialog.getExistingDirectory()
+    print(current_dir)
+    all_files = os.listdir(current_dir)
+    print(all_files)
+    image_files = images_select(all_files)
+    print(image_files)
+    list_widget_images.clear()
+    list_widget_images.addItems(image_files)
+
+def images_select(all_files):
+    result = []
+    extentions = ['.png', '.jpeg', '.jpg', '.gif']
+    
+    for file_name in all_files:
+        for ext in extentions:
+            if file_name.endswith(ext) or file_name.endswith(ext.upper()):
+                result.append(file_name)
+    return result
+
+def showChoosenImage():
+    name_image = list_widget_images.currentItem().text()
+    current_image.loadImage(current_dir, name_image)
+    current_image.showImage()
+
+
+btn_select_dir.clicked.connect(showFileNames)
+list_widget_images.currentRowChanged.connect(showChoosenImage)
 
 main_win.show()
 app.exec_()
